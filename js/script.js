7d8beae2c1f820032f0ddff83ddc833344cd0568
Peter <petr.liauos@gmail.com>
2022-11-30 14:05:49 +0800

    new play button for chinese chess

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-const textarray = ["Welcome, you are at NP Chess SIG","Welcome, you are at NP Chinese Chess SIG", "Welcome, you are at NP XiangQi SIG"]
-const index = Math.floor(Math.random() * 3);
-const text = textarray[list];
-for (let x = 0; x < text.length; x++){
-    document.querySelector('.maintext'),textcontent += text[x];
+/*having a form hidden*/
+function fromnp(that) {
+    if (that.value == "no") {
+alert("check");
+        document.getElementById("ifYes").style.display = "block";
+    }
+    else {
+        document.getElementById("ifYes").style.display = "none";
+    }
 }
